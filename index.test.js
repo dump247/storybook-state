6b6361fc945f163d2f00046fba4152b1b255e7ce
Cory Thomas <cthomas7577@gmail.com>
2017-11-28 11:43:13 -0600

    Change react dependency to v15

--- index.test.js
+++ index.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { withState } from './index';
 import { mount, configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from 'enzyme-adapter-react-15';
 
 configure({ adapter: new Adapter() });
 
